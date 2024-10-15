932c0d174cb946d1826c3d46f2ad78c782bcbfdb
Sumiya Jakariya <sumiyajakariya38299@gmail.com>
2024-10-15 18:56:33 +0600

    first code

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*Problem:Let's step into the world of Programming.
+
+In this problem you will just have to print the line "Hello world!" (without quotation)
+
+Input Format
+
+This problem doesn't have any input
+Output Format
+
+Print the line "Hello World!" (without quotation).
+Sample Output 0
+
+Hello World!*/
+
+#include<stdio.h>
+int main() 
+{
+
+    printf("Hello World!");
+    return 0;
+}
